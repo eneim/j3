1ff784b4182bcfecbf49d17f353b3fc5616fd7e9
eneim <mr.nguyenhoainam@gmail.com>
2013-05-08 00:08:10 +0900

    Update ex3_divide.asm

--- ex3_divide.asm
+++ ex3_divide.asm
@@ -1,23 +1,22 @@
-      	ORG 10		/ start
-LOP,	LDA Y		/ AC <- M[Y]
-	CMA	 	/ AC <- ~AC
-	INC	  	/ AC <- AC + 1 (AC = -M[Y])
+	ORG 10		/ start
+	LDA X
+LOP,	STA R		/ M[R] <- AC
+	LDA Y		/ AC <- M[Y]
+	CMA		/ AC <- ~AC
+	INC		/ AC <- AC + 1 (AC = -M[Y])
 	ADD X 		/ AC <- AC + M[X] (AC <- M[X] - M[Y])	
 	STA X		/ X <- AC
 	ISZ P		/ M[P]++
-	SNA	  	/ if (AC < 0) then skip next step
-	STA R		/ M[R] <- AC
-	SNA	 	/ if (AC < 0) then skip next step
+	SNA		/ if (AC < 0) then skip next step
 	BUN LOP		/ goto to LOP
-	CLA	  	/ AC <- 0	
+	CLA		/ AC <- 0	
 	LDA P		/ AC <- M[P]
-	CMA	  	/ AC <- ~AC
-	INC	  	/ AC <- AC + 1
-	CMA	  	/ AC <- ~AC
-	STA P		/ M[P] <- AC, M[P] <- M[P] - 1
-/ init data
-X,	DEC 300		/ M[X] = 300
-Y,	DEC 37		/ M[Y] = 37
-P,  	DEC 0		/ M[P] = 0
-R,	DEC 0		/ M[R] = 0
-	END	  	/ end
+	ADD S1		/ AC <- AC - 1
+	STA P		/ M[P] <- AC
+	HLT
+X,	DEC 32768	/ M[X] = 32768
+Y,	DEC 10000	/ M[Y] = 7935
+P,	DEC 0		/ M[P] init = 0
+R,	DEC 0		/ M[R] init = 0
+S1, 	DEC -1  	/ M[S1] = -1
+	END		/ end
