5e43411bbfb48bcefbec11e982f76148fb2c13a0
eneim <mr.nguyenhoainam@gmail.com>
2013-05-08 23:12:12 +0900

    Update ex3_maxprime.asm

--- ex3_maxprime.asm
+++ ex3_maxprime.asm
@@ -1,31 +1,30 @@
-  	ORG 10		/ start
+	ORG 10		/ start
 	LDA N
-	STA X
+	STA X		/ store N to X as a temporary variable
 	CIR
-	STA T		/ temp = N >> 1, test with N first
-/ loop 1: run the division
-LOP1,	CLA
-	LDA T		/ load temp variable
+	STA T		/ temp = N/2
+	SNA		/ if (T < 0) skip next step
+	BUN LOP1	/ goto LOP 1, compute X(N)%Y(T)
+	HLT		/ T < 0 then input has error, halt
+/ loop 1: compute N%Y
+LOP1,	LDA X
+	STA R		/ M[R] <- AC
+	LDA T		/ load temp variable, AC <- M[T]
 	STA Y		/ M[Y] = M[TMP]
-	CLA		/ AC <- 0
-	LDA Y		/ AC <- M[Y]
 	CMA		/ AC <- ~AC
 	INC		/ AC <- AC + 1 (AC = -M[Y])
 	ADD X 		/ AC <- AC + M[X] (AC <- M[X] - M[Y])	
 	STA X		/ X <- AC
 	SNA		/ if (AC < 0) then skip next step
-	STA R		/ M[R] <- AC
-	SNA		/ if (AC < 0) then skip next step
-	BUN LOP1	/ goto to LOP
-	LDA R
-	SZA		/ if R = 0 then X is not a prime, skip next step
+	BUN LOP1	/ goto to LOP1
+	LDA R		/ AC <- M[R]
+	SZA		/ if R = 0 skip next step
 	BUN LOP2	/ if R != 0 then test with Y = Y - 1
 	BUN LOP		/ R = 0 run LOP to check if Y = 1 or not
-/ loop 2: Y --, load N to X, test N/(Y-1)
+/ loop 2: Y = Y - 1, goto LOP1 again
 LOP2,	LDA Y
 	BSA SH4
 	STA T		/ Y = Y - 1
-	CLA
 	LDA N
 	STA X		/ X <- N
 	BUN LOP1
@@ -34,29 +33,29 @@ LOP2,	LDA Y
 / R = 0 then firstly check that if Y = 1 or not
 LOP,	LDA N
 	STA LAST
-	BSA SH4		/ N = N - 1, AC = M[N] - 1
+	BSA SH4		/ N = N - 1, AC <- M[N] - 1
 	STA N		/ M[N] <- AC
-	STA X
-	CIR
-	STA T
+	STA X		/ load N to X
+	CIR		/ AC <- M[X] >> 1
+	STA T		/ T = X/2
+	LDA Y		
+	BSA SH4		/ AC <- Y - 1
+	STA Y		/ Y = Y - 1
 	LDA Y
-	BSA SH4
-	STA Y
-	LDA Y
-	SZA		/ if Y = 0 then skip next step	
-	BUN LOP1	/ Y != 0 then test with N = N - 1
-	HLT
-/ sub routine : X = X - 1
+	SZA		/ if AC = 0 (Y = 1) then skip next step	
+	BUN LOP1	/ AC != 0 then test with N = N - 1
+	HLT		/ Y = 1, R = 0 then stop
+/ sub routine : X = X - 1 (special subtraction)
 SH4,	HEX 0
 	CMA		/ AC <- ~AC
 	INC		/ AC <- AC + 1
 	CMA		/ AC <- ~AC
 	BUN SH4 I	/ return from SH4
-/ end if sub routine
-LAST,	DEC 1		/ M[LAST] : the biggest prime number that is not exceed N
-N,	DEC 32770	/ M[N] = 32770
+/ init
+LAST,	DEC 1		/ M[LAST] : the biggest prime number that does not exceed N
+N,	DEC 32770	/ M[N] = 40, MAX N = 32770
 X,	DEC 1		/ M[X] = 1
 Y,	DEC 1		/ M[Y] = 1
-T,	DEC 0		/ temp
+T,	DEC 0		/ temp, init = 1
 R,	DEC 0		/ M[R] = 0, (Y, R) return (1, 0) then N is prime
 	END
