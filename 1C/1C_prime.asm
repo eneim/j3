a597a5c363db02b102b0191ef6567f2ad547c154
eneim <mr.nguyenhoainam@gmail.com>
2013-06-04 11:57:53 +0900

    Update 1C_prime.asm

--- 1C/1C_prime.asm
+++ 1C/1C_prime.asm
@@ -1,10 +1,11 @@
-  	ORG 10	/ start
+		ORG 10	/ start
 		LDA N 	/ first step
+		STA LAST
 		CIR
 		STA Y		/ temp = N/2, test with N first
-MAIN,	ADD M2		/ AC <- M[Y] - 2 (to check if Y >= 2 or not)
+MAIN,		ADD M2	/ AC <- M[Y] - 2 (to check if Y >= 2 or not)
 		SPA 		/ AC >= 0 then skip next
-		HLT
+		BUN OUTP	/ OUTPUT
 		BSA GETR	/ after this step, AC = M[R]
 		LDA R
 	// check R: 
@@ -54,6 +55,64 @@ RE_Y,	HEX 0
 		STA Y
 		BUN RE_Y I
 
+//////////////////// OUTPUT PROCESS /////////////////////
+OUTP,		HEX 0
+		LDA LAST
+		STA Z
+		LDA Z
+		BSA WRITE_Z
+		LDA VM5
+		STA CNT
+S_OUT,	SKO				/ if (S_OUT ready) -> skip next
+		BUN S_OUT			/ S_OUT not ready: goto L1
+		LDA A_ZMG I
+		OUT
+		ISZ A_ZMG		/ ++M[A_OUT]
+		ISZ CNT			/ if (++M[CNT]) skip next
+		BUN S_OUT			/ goto L1 (next output)
+		HLT				/ halt
+		BUN OUTP I
+
+WRITE_Z,	HEX 0		/ return address
+////////// subroutine (write Z to ZMG) //////////
+		LDA A_ZMG		/ AC <- M[A_ZMG] (ZMG)
+		ADD VH4			/ AC <- ZMG + 4
+		STA TMA			/ M[TMA] <- ZMG + 4
+		LDA VM5			/ AC <- M[VM4] (-4)
+		STA CNT			/ M[CNT] <- -4
+PUT_DGT,
+		LDA Z			/ AC <- M[Z]
+		BSA DV10
+		LDA R_O
+		ADD CH_0		/ AC <- (M[Z] & 000f) + '0'
+STR_DGT,
+		STA TMA I		/ M[M[TMA]] <- AC
+		LDA P
+		STA Z			/ M[Z] <- M[Z] >> 4
+		CLA
+		STA P
+		LDA TMA			/ AC <- M[TMA]
+		ADD VM1			/ AC <- M[TMA] - 1
+		STA TMA			/ M[TMA] <- M[TMA] - 1
+		ISZ CNT			/ ((++M[CNT]) = 0) ? skip next
+		BUN PUT_DGT		/ goto PUT_DGT
+		BUN WRITE_Z I	/ return from OUT_Z
+
+// compute TMX % 10 -> P, R
+DV10,	HEX 0
+		STA TMX
+LOP,	STA R_O		/ M[R] <- AC
+		ADD VM10	
+		STA TMX	/ X <- AC
+		ISZ P		/ M[P]++
+		SNA		/ if (AC < 0) then skip next step
+		BUN LOP	/ goto to LOP
+		CLA		/ AC <- 0	
+		LDA P		/ AC <- M[P]
+		ADD S1	/ AC <- AC - 1
+		STA P		/ M[P] <- AC
+		BUN DV10 I
+
 / init
 LAST,	DEC 0		/ M[LAST] : the biggest prime number that does not exceed N
 N,	DEC 999	/ M[N] = 999, MAX N = 32770
@@ -62,4 +121,26 @@ Y,	DEC 1		/ M[Y] = 1
 R,	DEC 0		/ M[R] = 0, (Y, R) return (1, 0) then N is prime
 M1,	DEC -1
 M2,	DEC -2
+Z,	DEC 0
+TMP,	DEC 0
+TMA,	HEX 0
+TMX,	DEC 0
+P,		DEC 0
+R_O,		DEC 0
+S1,		DEC -1
+CH_0,	HEX 30
+A_ZMG,	SYM ZMG
+ZMG,	HEX A		/ hex digit 4
+		HEX B		/ hex digit 3
+		HEX F		/ hex digit 2
+		HEX D		/ hex digit 1
+		HEX E 		/ hex digit 0
+VH4,	HEX 4		/ VH4 = 4
+VM1,	DEC -1		/ VM1 = -1
+VM4,	DEC -4		/ VM4 = -4
+VM5,	DEC -5		/ VM5 = -5
+VM10,	DEC -10		/ VM10 = -10
+CNT,	DEC 0		/ (init: -4) digit count
+AMKN,	HEX 000F	/ AMKN = 000F (and mask negated)
+
 	END
